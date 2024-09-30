6e24ff69eb81fdda205ef97f89fd327b3697aa80
Joel Riquelme <joel.riquelme@ug.uchile.cl>
2024-09-29 22:38:02 -0300

    feat(T2): addapt T1 to Flask

--- T2/flask_app/app.py
+++ T2/flask_app/app.py
@@ -5,3 +5,15 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("donations/index.html")
+
+@app.route("/agregar-donacion", methods=["GET", "POST"])
+def agregar_donacion():
+    return render_template("donations/agregar-donacion.html")
+
+@app.route("/ver-dispositivos", methods=["GET", "POST"])
+def ver_dispositivos():
+    return render_template("donations/ver-dispositivos.html")
+
+@app.route("/informacion-dispositivo", methods=["GET", "POST"])
+def informacion_dispositivo():
+    return render_template("donations/informacion-dispositivo.html")
--- T2/flask_app/static/js/donacion.js
+++ T2/flask_app/static/js/donacion.js
@@ -1,5 +1,5 @@
 // Cargar el JSON
-fetch('region_comuna.json')
+fetch('static/json/region_comuna.json')
     .then(response => response.json())
     .then(data => {
         const regionSelect = document.getElementById('region');
